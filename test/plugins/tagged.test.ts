7fee422b0fe8b8a34f9c9f13c1a0104710395d3a
Maksim Efremov <ma-efremoff@yandex-team.ru>
2023-09-29 14:04:32 +0300

    feat!: add settings.normalizeUrl
    https://github.com/ytsaurus/ytsaurus/issues/102
    
    Wrapping urls with h.yandex-team.ru works only for yandex-team authorzied users, so we need to replace it with
    another default behavior that will sanitize ulrs (encodeURI(url) looks good enough).
    Also we need to provide ability to implement previous behavior, so we add `settings.normalizeUrl` option.

--- test/plugins/tagged.test.ts
+++ test/plugins/tagged.test.ts
@@ -1,13 +1,19 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, test, beforeEach } from "@jest/globals";
 const unipika = require("../..")();
 
 describe("plugins", function () {
-  describe("tagged", function () {
-    var _format = unipika.format;
+  describe("tagged asHTML", () => {
+    let settings = {};
+
+    beforeEach(() => {
+      settings = {
+        asHTML: true,
+      };
+    });
 
     test("String url", function () {
       expect(
-        _format(
+        unipika.format(
           {
             $type: "tagged",
             $tag: "url",
@@ -16,18 +22,16 @@ describe("plugins", function () {
               $value: "http://example.com",
             },
           },
-          {
-            asHTML: true,
-          }
+          settings
         )
       ).toBe(
-        '<a class="yql_tagged tagged tag_url" target="_blank" href="https://h.yandex-team.ru/?http%3A%2F%2Fexample.com">http://example.com</a>'
+        '<a class="yql_tagged tagged tag_url" target="_blank" href="http://example.com">http://example.com</a>'
       );
     });
 
     test("Named url without text", function () {
       expect(
-        _format(
+        unipika.format(
           {
             $type: "tagged",
             $tag: "url",
@@ -38,18 +42,16 @@ describe("plugins", function () {
               },
             },
           },
-          {
-            asHTML: true,
-          }
+          settings
         )
       ).toBe(
-        '<a class="yql_tagged tagged tag_url" target="_blank" href="https://h.yandex-team.ru/?http%3A%2F%2Fexample.com">http://example.com</a>'
+        '<a class="yql_tagged tagged tag_url" target="_blank" href="http://example.com">http://example.com</a>'
       );
     });
 
     test("Named url with text", function () {
       expect(
-        _format(
+        unipika.format(
           {
             $type: "tagged",
             $tag: "url",
@@ -62,18 +64,16 @@ describe("plugins", function () {
               },
             },
           },
-          {
-            asHTML: true,
-          }
+          settings
         )
       ).toBe(
-        '<a class="yql_tagged tagged tag_url" target="_blank" href="https://h.yandex-team.ru/?http%3A%2F%2Fexample.com" title="Abc">Example</a>'
+        '<a class="yql_tagged tagged tag_url" target="_blank" href="http://example.com" title="Abc">Example</a>'
       );
     });
 
     test("Url with html special symbols", function () {
       expect(
-        _format(
+        unipika.format(
           {
             $type: "tagged",
             $tag: "url",
@@ -85,18 +85,219 @@ describe("plugins", function () {
               },
             },
           },
+          settings
+        )
+      ).toBe(
+        '<a class="yql_tagged tagged tag_url" target="_blank" href="http://example.com/?param1=%3C%3E%22&param2=%60\'">Example</a>'
+      );
+    });
+
+    test("String video url", function () {
+      expect(
+        unipika.format(
           {
-            asHTML: true,
-          }
+            $type: "tagged",
+            $tag: "videourl",
+            $value: {
+              $type: "yql.string",
+              $value: "http://example.com",
+            },
+          },
+          settings
+        )
+      ).toBe(
+        '<video class="yql_tagged tagged tag_video" controls src="http://example.com"></video>'
+      );
+    });
+
+    test("Struct tagged as video", function () {
+      expect(
+        unipika.format(
+          {
+            $type: "tagged",
+            $tag: "videourl",
+            $value: {
+              $type: "tag_value",
+              $value: {
+                src: "http://example.com",
+                width: 800,
+                height: 600,
+              },
+            },
+          },
+          settings
+        )
+      ).toBe(
+        '<video class="yql_tagged tagged tag_video" controls src="http://example.com" style="width:800px;height:600px"></video>'
+      );
+    });
+  });
+
+  describe("tagged asHTML with h.yandex-team.ru", function () {
+    let settings = {};
+
+    beforeEach(() => {
+      settings = {
+        asHTML: true,
+        normalizeUrl: (url) => {
+          return "https://h.yandex-team.ru/?" + encodeURIComponent(url);
+        },
+      };
+    });
+
+    test("String url", function () {
+      expect(
+        unipika.format(
+          {
+            $type: "tagged",
+            $tag: "url",
+            $value: {
+              $type: "yql.string",
+              $value: "http://example.com",
+            },
+          },
+          settings
+        )
+      ).toBe(
+        '<a class="yql_tagged tagged tag_url" target="_blank" href="https://h.yandex-team.ru/?http%3A%2F%2Fexample.com">http://example.com</a>'
+      );
+    });
+
+    test("Named url without text", function () {
+      expect(
+        unipika.format(
+          {
+            $type: "tagged",
+            $tag: "url",
+            $value: {
+              $type: "tag_value",
+              $value: {
+                href: "http://example.com",
+              },
+            },
+          },
+          settings
+        )
+      ).toBe(
+        '<a class="yql_tagged tagged tag_url" target="_blank" href="https://h.yandex-team.ru/?http%3A%2F%2Fexample.com">http://example.com</a>'
+      );
+    });
+
+    test("Named url with text", function () {
+      expect(
+        unipika.format(
+          {
+            $type: "tagged",
+            $tag: "url",
+            $value: {
+              $type: "tag_value",
+              $value: {
+                href: "http://example.com",
+                text: "Example",
+                title: "Abc",
+              },
+            },
+          },
+          settings
+        )
+      ).toBe(
+        '<a class="yql_tagged tagged tag_url" target="_blank" href="https://h.yandex-team.ru/?http%3A%2F%2Fexample.com" title="Abc">Example</a>'
+      );
+    });
+
+    test("Url with html special symbols", function () {
+      expect(
+        unipika.format(
+          {
+            $type: "tagged",
+            $tag: "url",
+            $value: {
+              $type: "tag_value",
+              $value: {
+                text: "Example",
+                href: "http://example.com/?param1=<>\"&param2=`'",
+              },
+            },
+          },
+          settings
         )
       ).toBe(
         '<a class="yql_tagged tagged tag_url" target="_blank" href="https://h.yandex-team.ru/?http%3A%2F%2Fexample.com%2F%3Fparam1%3D%3C%3E%22%26param2%3D%60\'">Example</a>'
       );
     });
 
+    test("String video url", function () {
+      expect(
+        unipika.format(
+          {
+            $type: "tagged",
+            $tag: "videourl",
+            $value: {
+              $type: "yql.string",
+              $value: "http://example.com",
+            },
+          },
+          settings
+        )
+      ).toBe(
+        '<video class="yql_tagged tagged tag_video" controls src="https://h.yandex-team.ru/?http%3A%2F%2Fexample.com"></video>'
+      );
+    });
+
+    test("Struct tagged as video", function () {
+      expect(
+        unipika.format(
+          {
+            $type: "tagged",
+            $tag: "videourl",
+            $value: {
+              $type: "tag_value",
+              $value: {
+                src: "http://example.com",
+                width: 800,
+                height: 600,
+              },
+            },
+          },
+          settings
+        )
+      ).toBe(
+        '<video class="yql_tagged tagged tag_video" controls src="https://h.yandex-team.ru/?http%3A%2F%2Fexample.com" style="width:800px;height:600px"></video>'
+      );
+    });
+  });
+
+  describe("tagged as text", () => {
+    let settings = {};
+    beforeEach(() => {
+      settings = {
+        asHTML: false,
+      };
+    });
+
+    test("Struct tagged as video raw", function () {
+      expect(
+        unipika.format(
+          {
+            $type: "tagged",
+            $tag: "videourl",
+            $value: {
+              $type: "tag_value",
+              $value: {
+                src: "http://example.com",
+                width: 800,
+                height: 600,
+              },
+            },
+          },
+          settings
+        )
+      ).toBe("http://example.com");
+    });
+
     test("Imageurl as plain output", function () {
       expect(
-        _format(
+        unipika.format(
           {
             $type: "tagged",
             $tag: "imageurl",
@@ -109,16 +310,14 @@ describe("plugins", function () {
               },
             },
           },
-          {
-            asHTML: false,
-          }
+          settings
         )
-      ).toBe("https://h.yandex-team.ru/?http%3A%2F%2Fexample.com");
+      ).toBe("http://example.com");
     });
 
     test("Image blob as plain output", function () {
       expect(
-        _format(
+        unipika.format(
           {
             $type: "tagged",
             $tag: "image/svg",
@@ -131,18 +330,16 @@ describe("plugins", function () {
               },
             },
           },
-          {
-            asHTML: false,
-          }
+          settings
         )
       ).toBe(
         "data:image/svg+xml;base64,&lt;svg&gt;&lt;circle r=&quot;2&quot; x=&quot;0&quot; y=&quot;0&quot;/&gt;&lt;/svg&gt;"
       );
     });
 
-    test("String video url", function () {
+    test("String video url raw", function () {
       expect(
-        _format(
+        unipika.format(
           {
             $type: "tagged",
             $tag: "videourl",
@@ -151,18 +348,88 @@ describe("plugins", function () {
               $value: "http://example.com",
             },
           },
+          settings
+        )
+      ).toBe("http://example.com");
+    });
+  });
+
+  describe("tagged as text with h.yandex-team.ru", () => {
+    let settings = {};
+    beforeEach(() => {
+      settings = {
+        asHTML: false,
+        normalizeUrl: (url) => {
+          return "https://h.yandex-team.ru/?" + encodeURIComponent(url);
+        },
+      };
+    });
+
+    test("Struct tagged as video raw", function () {
+      expect(
+        unipika.format(
           {
-            asHTML: true,
-          }
+            $type: "tagged",
+            $tag: "videourl",
+            $value: {
+              $type: "tag_value",
+              $value: {
+                src: "http://example.com",
+                width: 800,
+                height: 600,
+              },
+            },
+          },
+          settings
+        )
+      ).toBe("https://h.yandex-team.ru/?http%3A%2F%2Fexample.com");
+    });
+
+    test("Imageurl as plain output", function () {
+      expect(
+        unipika.format(
+          {
+            $type: "tagged",
+            $tag: "imageurl",
+            $value: {
+              $type: "tag_value",
+              $value: {
+                maxHeight: "200",
+                maxWidth: "200",
+                src: "http://example.com",
+              },
+            },
+          },
+          settings
+        )
+      ).toBe("https://h.yandex-team.ru/?http%3A%2F%2Fexample.com");
+    });
+
+    test("Image blob as plain output", function () {
+      expect(
+        unipika.format(
+          {
+            $type: "tagged",
+            $tag: "image/svg",
+            $value: {
+              $type: "tag_value",
+              $value: {
+                maxHeight: "200",
+                maxWidth: "200",
+                src: '<svg><circle r="2" x="0" y="0"/></svg>',
+              },
+            },
+          },
+          settings
         )
       ).toBe(
-        '<video class="yql_tagged tagged tag_video" controls src="https://h.yandex-team.ru/?http%3A%2F%2Fexample.com"></video>'
+        "data:image/svg+xml;base64,&lt;svg&gt;&lt;circle r=&quot;2&quot; x=&quot;0&quot; y=&quot;0&quot;/&gt;&lt;/svg&gt;"
       );
     });
 
     test("String video url raw", function () {
       expect(
-        _format(
+        unipika.format(
           {
             $type: "tagged",
             $tag: "videourl",
@@ -171,55 +438,7 @@ describe("plugins", function () {
               $value: "http://example.com",
             },
           },
-          {
-            asHTML: false,
-          }
-        )
-      ).toBe("https://h.yandex-team.ru/?http%3A%2F%2Fexample.com");
-    });
-
-    test("Struct tagged as video", function () {
-      expect(
-        _format(
-          {
-            $type: "tagged",
-            $tag: "videourl",
-            $value: {
-              $type: "tag_value",
-              $value: {
-                src: "http://example.com",
-                width: 800,
-                height: 600,
-              },
-            },
-          },
-          {
-            asHTML: true,
-          }
-        )
-      ).toBe(
-        '<video class="yql_tagged tagged tag_video" controls src="https://h.yandex-team.ru/?http%3A%2F%2Fexample.com" style="width:800px;height:600px"></video>'
-      );
-    });
-
-    test("Struct tagged as video raw", function () {
-      expect(
-        _format(
-          {
-            $type: "tagged",
-            $tag: "videourl",
-            $value: {
-              $type: "tag_value",
-              $value: {
-                src: "http://example.com",
-                width: 800,
-                height: 600,
-              },
-            },
-          },
-          {
-            asHTML: false,
-          }
+          settings
         )
       ).toBe("https://h.yandex-team.ru/?http%3A%2F%2Fexample.com");
     });
